0911609adf0c16554131d57f352176b559342b74
Una Kravets <una.kravets@gmail.com>
2018-04-08 00:40:08 -0400

    styling homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,52 @@
 import React from 'react'
 import Link from 'gatsby-link'
 import Overdrive from 'react-overdrive'
-import bender from "../media/bender.jpg";
+import bookCover from '../media/7319052-L.jpg';
+import bookCover2 from '../media/473600-L.jpg';
+import style from './index.css'
 
 const IndexPage = () => (
-  <div>
-    <h1>Hi people</h1>
-    <Overdrive id="bender-to-big-fry">
-      <img src={bender} width="100" height="100" />
-    </Overdrive>
-    <p>This is the Home component with the full book list.</p>
-    <Link to="/page-2/">Go to page 2</Link>
-  </div>
+  <ul className="bookList">
+    <li className="bookListItem">
+      <div className="book">
+        <Link to="/page-2/">
+          <div className="bookCoverContainer">
+            <Overdrive id="bookCover-to-page">
+              <img src={bookCover} className="bookCover"/>
+            </Overdrive>
+          </div>
+          <Overdrive id="bookCover-to-page--title">
+            <h2>Creativity, Inc.: Overcoming the Unseen Forces That Stand in the Way of True Inspiration</h2>
+          </Overdrive>
+          <span className="stars four">⭐️⭐️⭐️⭐️⭐️</span>
+        </Link>
+      </div>
+    </li>
+    <li className="bookListItem">
+      <div className="book">
+        <Link to="/page-3/">
+          <div className="bookCoverContainer">
+            <Overdrive id="bookCover-to-page2">
+              <img src={bookCover2} className="bookCover" />
+            </Overdrive>
+          </div>
+          <h2>The Glass Castle</h2>
+          <span className="stars five">⭐️⭐️⭐️⭐️⭐️</span>
+        </Link>
+      </div>
+    </li>
+    <li className="bookListItem">
+      <div className="book">
+        <Link to="/page-2/">
+          <div className="bookCoverContainer">
+              <img src={bookCover} className="bookCover" />
+          </div>
+            <h2>Creativity, Inc.: Overcoming the Unseen Forces That Stand in the Way of True Inspiration</h2>
+          <span className="stars fourHalf">⭐️⭐️⭐️⭐️⭐️</span>
+        </Link>
+      </div>
+    </li>
+  </ul>
 )
 
 export default IndexPage
--- src/pages/page-3.js
+++ src/pages/page-3.js
@@ -1,14 +1,12 @@
 import React from 'react'
 import Link from 'gatsby-link'
 import Overdrive from 'react-overdrive'
-import bookCover from '../media/7319052-L.jpg';
+import bookCover from '../media/473600-L.jpg';
 
 const SecondPage = () => (
   <div>
-    <Overdrive id="bookCover-to-page--title">
-      <h1>Creativity, Inc.: Overcoming the Unseen Forces That Stand in the Way of True Inspiration</h1>
-    </Overdrive>
-    <Overdrive id="bookCover-to-page">
+    <h1>Hi from the second page</h1>
+    <Overdrive id="bookCover-to-page2">
       <img src={bookCover} />
     </Overdrive>
     <p>Welcome to page 2</p>
